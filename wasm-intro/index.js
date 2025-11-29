a70532e0f6994cff1978a8b4103be03ee5326fa5
Ilya Kirsanov <ilya.kirsanov@manychat.com>
2025-11-29 23:05:14 +0100

    chore

--- wasm-intro/index.js
+++ wasm-intro/index.js
@@ -2,19 +2,24 @@ class App {
   constructor() {}
 
   async factorial() {
-    const wasm = await WebAssembly.instantiateStreaming(await fetch('factorial.wasm'));
+    const wasm = await WebAssembly.instantiateStreaming(
+      await fetch('/wasm-intro/factorial/factorial.wasm'),
+    );
     console.log(wasm.instance.exports.factorial(5));
   }
 
   async hello() {
-    const wasm = await WebAssembly.instantiateStreaming(await fetch('hello.wasm'), {
-      env: {
-        log: (from, to) => {
-          const memory = wasm.instance.exports.memory;
-          console.log(new TextDecoder().decode(memory.buffer.slice(from, to)));
+    const wasm = await WebAssembly.instantiateStreaming(
+      await fetch('/wasm-intro/hello-world/hello.wasm'),
+      {
+        env: {
+          log: (from, to) => {
+            const memory = wasm.instance.exports.memory;
+            console.log(new TextDecoder().decode(memory.buffer.slice(from, to)));
+          },
         },
       },
-    });
+    );
 
     wasm.instance.exports.hello();
   }
--- wasm-intro/node.js
+++ wasm-intro/node.js
@@ -1,6 +1,6 @@
 // !!! IMPORTANT: Direct WASM imports require Node.js v24.11.0+
-import { factorial } from './factorial.wasm';
-import { sum } from './sum.wasm';
+import { factorial } from './factorial/factorial.wasm';
+import { sum } from './sum/sum.wasm';
 
 console.log(factorial(5));
 console.log(sum(1, 1));
