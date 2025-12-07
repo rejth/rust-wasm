d7bb1f3481d8aa5864f65448c3047f93de0660c0
Ilya Kirsanov <ilya.kirsanov@manychat.com>
2025-12-07 23:22:00 +0100

    lesson 9: set up rust for web

---
+++ img-ops/js/index.js
@@ -0,0 +1,3 @@
+import init from '../pkg/index.js';
+
+init().catch(console.error);
---
+++ img-ops/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+
+const dist = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './js/index.js',
+  },
+  output: {
+    path: dist,
+    filename: '[name].js',
+  },
+  devServer: {
+    static: dist,
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'static' }],
+    }),
+
+    new WasmPackPlugin({
+      crateDirectory: __dirname,
+      extraArgs: '--target web',
+    }),
+  ],
+};
