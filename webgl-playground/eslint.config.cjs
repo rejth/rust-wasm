bd05e7865d32a46386003d0057142fae23abf572
Ilya Kirsanov <ilya.kirsanov@manychat.com>
2025-12-21 19:11:47 +0100

    WebGL playground: chore

---
+++ webgl-playground/eslint.config.cjs
@@ -0,0 +1,57 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const { configs: jsConfigs } = require('@eslint/js/src');
+
+const browserGlobals = {
+  console: 'readonly',
+  document: 'readonly',
+  window: 'readonly',
+  navigator: 'readonly',
+  location: 'readonly',
+  webglUtils: 'readonly',
+};
+
+const ignorePatterns = [
+  'node_modules',
+  'dist',
+  'pkg',
+  'target',
+  'src',
+  'tests',
+  'static',
+  'eslint.config.cjs',
+];
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: jsConfigs.recommended,
+  allConfig: jsConfigs.all,
+});
+
+module.exports = [
+  {
+    ignores: ignorePatterns,
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ),
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      globals: { ...browserGlobals },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
