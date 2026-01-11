e574bae600bc9fb2bfab54c144da9f4de0bccaf8
Ilya Kirsanov <ilya.kirsanov@manychat.com>
2026-01-11 23:22:16 +0100

    WGPU sandbox: add surface and render pass

--- wgpu-sandbox/src/lib.rs
+++ wgpu-sandbox/src/lib.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use winit::{
     application::ApplicationHandler,
+    dpi::PhysicalPosition,
     event::*,
     event_loop::{ActiveEventLoop, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
@@ -19,18 +20,181 @@ fn log(message: &str) {
 }
 
 pub struct State {
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
     window: Arc<Window>,
+    clear_color: wgpu::Color,
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+        let size = window.inner_size();
+
+        // The instance is a handle to our GPU.
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU.
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            #[cfg(not(target_arch = "wasm32"))]
+            backends: wgpu::Backends::PRIMARY,
+            #[cfg(target_arch = "wasm32")]
+            backends: wgpu::Backends::GL,
+            ..Default::default()
+        });
+
+        // The surface is the part of the window that we draw to.
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        // The adapter is a handle for our actual graphics card.
+        // We request an adapter that is compatible with our surface.
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                experimental_features: wgpu::ExperimentalFeatures::disabled(),
+                // "required_limits" describes the limit of certain types of resources that we can create.
+                // WebGL doesn't support all of wgpu's features, so if we're building for the web we'll have to disable some.
+                required_limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                // The "memory_hints" field provides the adapter with a preferred memory allocation strategy.
+                // This is used to optimize the allocation of memory for the resources.
+                memory_hints: Default::default(),
+                // "trace" describes the tracing level of the device.
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+        // Shader code assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        // The config is the configuration of the surface.
+        // This will define how the surface creates its underlying SurfaceTexture.
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT, // Describes how SurfaceTextures will be used. RENDER_ATTACHMENT specifies that the textures will be used to write to the screen.
+            format: surface_format, // Defines how SurfaceTextures will be stored on the GPU. The format of the surface - sRGB surface texture.
+            width: size.width,      // The width of the surface in pixels.
+            height: size.height,    // The height of the surface in pixels.
+            present_mode: surface_caps.present_modes[0], // Determines how to sync the surface with the display. FIFO (first in, first out) ensures that frames are displayed in the order they are submitted.
+            alpha_mode: surface_caps.alpha_modes[0], // The alpha mode of the surface. It has something to do with transparent windows
+            view_formats: vec![], // List of TextureFormats that you can use when creating TextureViews.
+            desired_maximum_frame_latency: 2, // The desired maximum frame latency of the surface.
+        };
+
+        Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+            window,
+            clear_color: wgpu::Color::BLACK,
+        })
     }
 
-    pub fn resize(&mut self, _width: u32, _height: u32) {}
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+            self.surface.configure(&self.device, &self.config);
+            self.is_surface_configured = true;
+        }
+    }
 
-    pub fn render(&mut self) {
+    fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        match (code, is_pressed) {
+            (KeyCode::Escape, true) => event_loop.exit(),
+            _ => {}
+        }
+    }
+
+    fn handle_mouse_moved(&mut self, position: PhysicalPosition<f64>) {
+        self.clear_color.r = position.x / self.config.width as f64;
+        self.clear_color.g = position.y / self.config.height as f64;
+    }
+
+    fn update(&mut self) {
+        // TODO: Implement update logic
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         self.window.request_redraw();
+
+        // We can't render unless the surface is configured
+        if !self.is_surface_configured {
+            return Ok(());
+        }
+
+        // Get the current texture and create a view for it. It is a texture that receives the output of any drawing commands performed.
+        let output = self.surface.get_current_texture()?;
+        let texture_view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        // Command encoder is used to record all the drawing commands that will be executed by the GPU.
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        {
+            /*
+             * Begin a render pass to clear the canvas.
+             * Render pass is when all drawing operations in GPU happen.
+             * Render pass may have several textures, called attachments, with various purposes such as storing the depth of rendered geometry or providing antialiasing.
+             */
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                // The "color_attachments" field describes where we are going to draw our color data to.
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &texture_view,  // informs wgpu what texture to save the colors to
+                    resolve_target: None, // it is the texture that will receive the resolved output. It is used to resolve the multisampled texture to a single sample texture.
+                    depth_slice: None,
+                    // This tells wgpu what to do with the colors on the screen
+                    ops: wgpu::Operations {
+                        // The "load" field tells wgpu how to handle colors stored from the previous frame. In out setup we want to clear the texture to a specific color.
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        // Store the result of the render pass in the texture
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+                multiview_mask: None,
+            });
+        }
+
+        /*
+         * Submit the command buffer to the GPU.
+         * First finish the command encoder and get the command buffer to be submitted to the GPU.
+         * Then submit the command buffer to the GPU. The queue performs all GPU commands, ensuring that their execution is well ordered and properly synchronized.
+         * Once you submit a command buffer, it cannot be used again, so you need to create a new one for other commands
+         */
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -134,8 +298,22 @@ impl ApplicationHandler<State> for App {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
             WindowEvent::Resized(size) => state.resize(size.width, size.height),
+            WindowEvent::CursorMoved { position, .. } => state.handle_mouse_moved(position),
             WindowEvent::RedrawRequested => {
-                state.render();
+                state.update();
+                match state.render() {
+                    Ok(_) => {
+                        log::info!("Rendered");
+                    }
+                    // Reconfigure the surface if it's lost or outdated
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        let size = state.window.inner_size();
+                        state.resize(size.width, size.height);
+                    }
+                    Err(e) => {
+                        log::error!("Unable to render {}", e);
+                    }
+                }
             }
             WindowEvent::KeyboardInput {
                 event:
@@ -145,10 +323,7 @@ impl ApplicationHandler<State> for App {
                         ..
                     },
                 ..
-            } => match (code, key_state.is_pressed()) {
-                (KeyCode::Escape, true) => event_loop.exit(),
-                _ => {}
-            },
+            } => state.handle_key(event_loop, code, key_state.is_pressed()),
             _ => {}
         }
     }
@@ -169,6 +344,7 @@ pub fn run() -> anyhow::Result<()> {
         #[cfg(target_arch = "wasm32")]
         &event_loop,
     );
+
     event_loop.run_app(&mut app)?;
 
     Ok(())
